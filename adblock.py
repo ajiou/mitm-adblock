3576413a96654527b656412beb14c178642c3b9d
ajiou <playpca9@gmail.com>
2022-08-05 17:06:03 +0800

    reformat and upgrade

--- adblock.py
+++ adblock.py
@@ -1,57 +1,47 @@
-"""
-An mitmproxy adblock script!
-(Required python modules: re2 and adblockparser)
-
-(c) 2015-2019 epitron
-"""
-
-import re2
-from mitmproxy.script import concurrent
-from mitmproxy import http
-from adblockparser import AdblockRules
 from glob import glob
+import re2
+from adblockparser import AdblockRules
+from mitmproxy import http
+from mitmproxy.script import concurrent
 
 __version__ = '0.1.0'
 
-IMAGE_MATCHER      = re2.compile(r"\.(png|jpe?g|gif)$")
-SCRIPT_MATCHER     = re2.compile(r"\.(js)$")
+IMAGE_MATCHER = re2.compile(r"\.(png|jpe?g|gif)$")
+SCRIPT_MATCHER = re2.compile(r"\.(js)$")
 STYLESHEET_MATCHER = re2.compile(r"\.(css)$")
+
 
 def log(msg):
     print(msg)
 
+
 def combined(filenames):
-  '''
-  Open and combine many files into a single generator which returns all
-  of their lines. (Like running "cat" on a bunch of files.)
-  '''
-  for filename in filenames:
-    with open(filename) as file:
-      for line in file:
-        yield line
+    for filename in filenames:
+        with open(filename) as file:
+            for line in file:
+                yield line
 
 
-def load_rules(blocklists=None):
-    rules = AdblockRules(
-        combined(blocklists),
+def load_rules(blockLists=None):
+    parsed = AdblockRules(
+        combined(blockLists),
         use_re2=True,
-        max_mem=512*1024*1024
-        # supported_options=['script', 'domain', 'image', 'stylesheet', 'object']
+        max_mem=512 * 1024 * 1024
     )
 
-    return rules
+    return parsed
 
 
 blocklists = glob("blocklists/*")
 
 if len(blocklists) == 0:
-  log("Error, no blocklists found in 'blocklists/'. Please run the 'update-blocklists' script.")
-  raise SystemExit
+    log("Error, no blocklists found in 'blocklists/'. Please run the 'update-blocklists' script.")
+    raise SystemExit
 
 else:
-  log("* Available blocklists:")
-  for list in blocklists:
-    log("  |_ %s" % list)
+    log("* Available blocklists:")
+    for i in blocklists:
+        log("  |_ %s" % i)
 
 log("* Loading blocklists...")
 rules = load_rules(blocklists)
